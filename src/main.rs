f68947c8e081f5569eb6170c327c4480c87a9d18
Lefteris Georgatos <lefteris.georgatos@persado.com>
2024-05-10 14:16:20 +0300

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,38 @@
+use std::io::Error;
+use std::process::{Child, ChildStdin, Command, Output, Stdio,};
+
+const K_ARGS: [&str; 3] = ["get", "pods", "--output=name"];
+const FZF_ARGS: [&str; 2] = ["--height=60%", "--reverse"];
+
+fn fzf_cmd() -> Command {
+    Command::new("fzf")
+}
+fn k_cmd() -> Command {
+    Command::new("kubectl")
+}
+fn fzf(args: std::slice::Iter<&str>) -> Result<Child, Error> {
+    fzf_cmd().args(args).stdin(Stdio::piped()).stdout(Stdio::piped()).spawn()
+}
+fn push_pods(args: std::slice::Iter<&str>, stdin: ChildStdin) -> Output {
+    k_cmd().args(args).stdout(stdin).output().unwrap()
+}
+fn run_shell(pod: &str, cmd: &str) -> Child {
+    k_cmd().args(["exec", "-it", pod, "--", cmd]).spawn().expect("some error")
+}
+
+fn main() {
+    let mut fzf = fzf(FZF_ARGS.iter()).expect("with error");
+
+    push_pods(K_ARGS.iter(), fzf.stdin.take().unwrap());
+
+    let results = fzf.wait_with_output().unwrap();
+
+    if let Some(0) = results.status.code() {
+        let pod = &String::from_utf8_lossy(&results.stdout).replace("\n", "");
+        let cmd = "/bin/bash";
+        let mut cmd: Child = run_shell(pod, cmd);
+        let status = cmd.wait();
+        println!("Exited with status {:?}", status);
+    }
+}
+
